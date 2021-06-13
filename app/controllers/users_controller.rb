bbd8ea160398f8bcf4c54aa3b4bf9a3776ed0e13
kohei <shoot0321@gmail.com>
2021-06-13 18:36:20 +0900

    詳細表示機能showを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,22 @@
 class UsersController < ApplicationController
+  #ユーザー一覧を表示するアクション
   def index
     @users = User.all
   end
 
+  #新規作成画面に対応するアクション
   def new
     @user = User.new
   end
 
+  #新規登録するためのアクション
   def create
     User.create(user_params)
+  end
+
+  #詳細情報を表示するアクション
+  def show
+    @user = User.find(params[:id])
   end
 
   private
